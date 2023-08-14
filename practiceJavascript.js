93ccc9ea9a62ef2781faa862db83da500ee2d80d
iamnaymur <nrtusher1@gmail.com>
2023-08-14 06:39:26 +0600

    first

---
+++ practiceJavascript.js
@@ -0,0 +1,12 @@
+/*JS Events, 
+Data Types,
+Array,
+Objects,
+Loop,
+Debugging,
+Events,
+Scope,
+Ajax,
+Promises, 
+DOM manipulationES6 features: let, const, Arrow Functions, Array Methods etc.
+*/
